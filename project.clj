a80ed49f303a8a4b89698961060bcaa3dc4f0d87
Aleksander Sumowski <asumowsk@thoughtworks.com>
2014-03-24 15:29:20 +0000

    sample midje project

--- project.clj
+++ project.clj
@@ -1,8 +1,5 @@
-(defproject te "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"] ]
-  :profiles  {:dev  {:dependencies  [[midje "1.5.1"]]}}
-  )
+(defproject test_midje "0.0.1-SNAPSHOT"
+  :description "Cool new project to do things and stuff"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
+  
--- test/te/core_test.clj
+++
@@ -1,9 +0,0 @@
-(ns te.t-core
-    (:use midje.sweet)
-    (:require  [te.core :as core]))
-
-(fact "Sample test"
-        (str/split "a/b/c" #"/") =>  ["a" "b" "c"]
-       )
-
-
